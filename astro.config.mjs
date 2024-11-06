82b4404bf940d13a0fb221b47a45dd3ebf48ee92
Yuki <128908897+yukazakiri@users.noreply.github.com>
2024-11-06 20:44:40 +0800

    used base.css for the tailwind and fixed the styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        heroicons: ["*"],
+        // You can add more icon sets here
+      }
+    })
+  ],
 });
